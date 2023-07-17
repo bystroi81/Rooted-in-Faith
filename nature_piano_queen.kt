b42ffe4bc9670b6e51ce88629da16d100f21c84f
bystroi81 <136693674+bystroi81@users.noreply.github.com>
2023-07-17 12:49:59 +0700

    Fix memory leaks

---
+++ nature_piano_queen.kt
@@ -0,0 +1,422 @@
+//1
+fun main(){
+    println("Rooted in Faith")
+}
+
+//2
+fun readVerse() {
+    println("Let us hold fast the confession of our hope without wavering, for He who promised is faithful")
+}
+
+//3
+fun getFaithful() {
+    println("For we have been made partakers of Christ, if we hold the beginning of our confidence steadfast to the end")
+}
+
+//4
+fun emphAcrostic(){
+    println("F – Firmly Fastened")
+    println("A – Always Attentive")
+    println("I – Incredibly Impactful")
+    println("T – Totally Trustworthy")
+    println("H – Heavily Held")
+}
+
+//5
+fun christFollower(){
+    println("Therefore, since we are surrounded by so great a cloud of witnesses, let us also lay aside every weight, and sin which clings so closely")
+}
+
+//6
+fun faithOnDisplay(){
+    println("And let us run with endurance the race that is set before us, looking to Jesus, the founder and perfecter of our faith")
+}
+
+//7
+fun abideInFaith(){
+    println("For our God is faithful. He will not forsake us but will help us and deliver us")
+}
+
+//8
+fun faithLeads(){
+    println("Our hope is not in ourselves but in Jesus who is the author and perfecter of our faith")
+}
+
+//9
+fun faithRewards(){
+    println("Therefore, let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up")
+}
+
+//10
+fun faithHolding(){
+    println("Peace will guard your hearts and minds as you live in harmony with one another and abide in the truth of the Lord Jesus Christ.")
+}
+
+//11
+fun faithFighter(){
+    println("Let us fight the good fight of faith and lay hold on eternal life to which we were called")
+}
+
+//12
+fun faithConfession(){
+    println("Let us hold fast the confession of our hope without wavering, for He who promised is faithful")
+}
+
+//13
+fun faithEndures(){
+    println("Therefore, since we have a great high priest who has passed through the heavens, Jesus the Son of God, let us hold firmly to the faith we profess")
+}
+
+//14
+fun faithHope(){
+    println("Let us be diligent to enter that rest, lest anyone fall according to the same example of disobedience")
+}
+
+//15
+fun faithRooted(){
+    println("Therefore, let us be rooted and established in love knowing that we have been born again, not of perishable seed but of imperishable, through the living and abiding word of God")
+}
+
+//16
+fun faithGrowing(){
+    println("He who began a good work in us will bring it to completion at the day of Jesus Christ")
+}
+
+//17
+fun faithFruitful(){
+    println("Therefore, being rooted and grounded in love, may be able to comprehend with all the saints what is the breadth, length, depth, and height of the love of Christ")
+}
+
+//18
+fun faithKnowing(){
+    println("For in Him all the fullness of God dwells in bodily form, and in Him you are made complete")
+}
+
+//19
+fun faithInto(){
+    println("For in Him we live and move and have our being, as also some of your own poets have said, 'For we are also His offspring")
+}
+
+//20
+fun faithCalled(){
+    println("Called unto His marvellous light, who once were not a people, but are now the people of God")
+}
+
+//21
+fun faithHope3(){
+    println("Through the Lord, we are a royal priesthood, a holy nation, a peculiar people, that we may show forth the praises of Him who has called us out of darkness into His marvellous light")
+}
+
+//22
+fun faithIncrease(){
+    println("I know whom I have believed, and am persuaded that He is able to keep that which I have committed unto Him against that day")
+}
+
+//23
+fun faithExamples(){
+    println("Do not be conformed to this world, but be transformed by the renewing of your mind, that you may prove what is that good and acceptable and perfect will of God")
+}
+
+//24
+fun faithEnduring(){
+    println("Therefore we also, since we are surrounded by so great a cloud of witnesses, let us lay aside every weight, and the sin which so easily ensnares us, and let us run with endurance the race that is set before us")
+}
+
+//25
+fun faithProtect(){
+    println("And let us not be worn down with worry, but guard our hearts in Christ Jesus")
+}
+
+//26
+fun faithStrength(){
+    println("Therefore let us be strong in the Lord and in the power of His might. Put on the whole armor of God, that you may be able to stand against the wiles of the devil")
+}
+
+//27
+fun faithAbide(){
+    println("Rest in the Lord, and wait patiently for Him. Do not fret because of him who prospers in his way, because of the man who brings wicked schemes to pass")
+}
+
+//28
+fun faithRestore(){
+    println("The Lord makes all things new. He shall bring forth your righteousness as the light, and your justice as the noonday")
+}
+
+//29
+fun faithReminders(){
+    println("Remember, the Lord is faithful and just to forgive us our sins and to cleanse us from all unrighteousness")
+}
+
+//30
+fun faithIlluminate(){
+    println("For the Lord is our light and salvation, whom shall we fear? The Lord is the stronghold of our life. No good thing will He withhold from those who walk uprightly")
+}
+
+//31
+fun faithEyes(){
+    println("We fix our eyes not on what is seen, but on what is unseen, for what is seen is temporary, but what is unseen is eternal")
+}
+
+//32
+fun faithShine(){
+    println("Let us shine forth His light as we walk in a manner worthy of the calling with which you have been called. Abide in Him, and He in you")
+}
+
+//33
+fun faithShield(){
+    println("Therefore, put on the whole armor of God, that you may be able to withstand in the evil day, and having done all, to stand")
+}
+
+//34
+fun faithTestify(){
+    println("Let your light so shine before men, that they may see your good works, and glorify your Father in heaven")
+}
+
+//35
+fun faithCleansed(){
+    println("If we confess our sins, He is faithful and just to forgive us our sins and to cleanse us from all unrighteousness")
+}
+
+//36
+fun faithFellowship(){
+    println("Therefore, let us consider one another in order to stir up love and good works, and exhorting one another, and so much the more as you see the Day approaching")
+}
+
+//37
+fun faithBasis(){
+    println("For the Word of God is living and powerful, and sharper than any two-edged sword, piercing even to the division of soul and spirit, and of joints and marrow, and is a discerner of the thoughts and intents of the heart")
+}
+
+//38
+fun faithUnity(){
+    println("Finally, let us be perfectly united in mind and thought. Be sympathetic, love as brothers, be compassionate and humble")
+}
+
+//39
+fun faithProtect2(){
+    println("Be kind and compassionate to one another, forgiving each other, just as in Christ God forgave you")
+}
+
+//40
+fun faithAdherence(){
+    println("Let us hold fast the confession of our hope without wavering, for He who promised is faithful")
+}
+
+//41
+fun faithBalm(){
+    println("Let us therefore come boldly to the throne of grace, that we may obtain mercy and find grace to help in time of need")
+}
+
+//42
+fun faithVictory(){
+    println("For the weapons of our warfare are not carnal but are mighty in God for pulling down strongholds, casting down arguments and every high thing that exalts itself against the knowledge of God")
+}
+
+//43
+fun faithReward(){
+    println("Therefore, my beloved brethren, be steadfast, immovable, always abounding in the work of the Lord, knowing that your labor is not in vain in the Lord")
+}
+
+//44
+fun faithSurrounded(){
+    println("For He Himself has said, 'I will never leave you nor forsake you'. So we may boldly say: 'The Lord is my helper; I will not fear. What can man do to me?'")
+}
+
+//45
+fun faithBreadth(){
+    println("May you be able to comprehend with all the saints what is the width and length and depth and height-- to know the love of Christ which passes knowledge; that you may be filled with all the fullness of God")
+}
+
+//46
+fun faithGrafted(){
+    println("Blessed be the God and Father of our Lord Jesus Christ, who has blessed us with every spiritual blessing in the heavenly places in Christ")
+}
+
+//47
+fun faithRedeemer(){
+    println("In Him we have redemption through His blood, the forgiveness of sins, according to the riches of His grace")
+}
+
+//48
+fun faithAnchor(){
+    println("Therefore, since we have been justified through faith, we have peace with God through our Lord Jesus Christ")
+}
+
+//49
+fun faithWait(){
+    println("Let us wait for the Lord; be strong and take heart and wait for the Lord")
+}
+
+//50
+fun faithDiligent(){
+    println("Let us be diligent to make our call and election sure, for if we do these things we will never stumble")
+}
+
+//51
+fun faithStability(){
+    println("Let us remain steady in faith, not being moved away from the hope of the gospel")
+}
+
+//52
+fun faithFoundation(){
+    println("Therefore, let us be steadfast, immovable, always abounding in the work of the Lord, knowing that in the Lord our labour is not in vain")
+}
+
+//53
+fun faithJoy(){
+    println("Let us rejoice in the Lord, we should be glad and shout for joy")
+}
+
+//54
+fun faithTruth(){
+    println("The Lord is our light and salvation; whom shall we fear? The Lord is the strength of our life; of whom shall we be afraid")
+}
+
+//55
+fun faithSecure(){
+    println("Let us be confident of this very thing, that He who has begun a good work in us will complete it until the day of Jesus Christ")
+}
+
+//56
+fun faithLight(){
+    println("Let us walk as children of light, finding out what is acceptable to the Lord")
+}
+
+//57
+fun faithLove(){
+    println("Let us love one another, for love is of God; and everyone who loves is born of God and knows God")
+}
+
+//58
+fun faithSurrounded2(){
+    println("For God has not given us a spirit of fear, but of power and of love and of a sound mind")
+}
+
+//59
+fun faithStrengthen(){
+    println("Therefore, let us be strong in the Lord and in the power of His might")
+}
+
+//60
+fun faithCalled2(){
+    println("Let us be called by His name, and do not depart from His commandments")
+}
+
+//61
+fun faithThrone(){
+    println("Let us draw near with a true heart in full assurance of faith, having our hearts sprinkled from an evil conscience and our bodies washed with pure water")
+}
+
+//62
+fun faithConfidence(){
+    println("Let us approach the throne of grace with confidence, so that we may receive mercy and find grace to help us in our time of need")
+}
+
+//63
+fun faithHelp(){
+    println("For the Lord is a shelter for the oppressed, a refuge in times of trouble")
+}
+
+//64
+fun faithPleasure(){
+    println("Let us take pleasure in the Lord, and He will give us the desires of our heart")
+}
+
+//65
+fun faithIncredible(){
+    println("Let us remember that with God all things are possible")
+}
+
+//66
+fun faithEntrust(){
+    println("Let us cast all our anxiety on Him, for He cares for us")
+}
+
+//67
+fun faithGuide(){
+    println("For the Lord will be your everlasting light, and your days of sorrow will end")
+}
+
+//68
+fun faithJourney(){
+    println("Let us remember that faith is the assurance of things hoped for, the conviction of things not seen")
+}
+
+//69
+fun faithComposed(){
+    println("Therefore, be still and know that I am God. I will be exalted among the nations, I will be exalted in the earth")
+}
+
+//70
+fun faithLifted(){
+    println("Let us not be anxious about anything, but in everything by prayer and supplication, with thanksgiving, let us make our requests known to God")
+}
+
+//71
+fun faithInnocence(){
+    println("Let us remember that God is our refuge and strength, a very present help in trouble")
+}
+
+//72
+fun faithPrayers(){
+    println("Let us pray without ceasing, in everything giving thanks; for this is the will of God in Christ Jesus for you")
+}
+
+//73
+fun faithCulture(){
+    println("Let us not be conformed to this world, but be transformed by the renewing of our mind, that we may prove what is that good and acceptable and perfect will of God")
+}
+
+//74
+fun faithFruit(){
+    println("Let us bear much fruit, showing ourselves to be His disciples")
+}
+
+//75
+fun faithHope2(){
+    println("Let us hold fast the confession of our hope without wavering, for He who promised is faithful")
+}
+
+//76
+fun faithEncourage(){
+    println("Let us encourage one another and build one another up, just as in fact you are doing")
+}
+
+//77
+fun faithSigns(){
+    println("Let us remember the signs of the times, for we do not know when the time will come")
+}
+
+//78
+fun faithWrath(){
+    println("Let us not be taken by surprise, like the rest of mankind, by the Lord's impending wrath")
+}
+
+//79
+fun faithPlace(){
+    println("Let us know that our place is secure in Him, for He loves us with an everlasting love")
+}
+
+//80
+fun faithName(){
+    println("Therefore, let us be known as His, for He has chosen us out of the world")
+}
+
+//81
+fun faithWorship(){
+    println("Let us worship Him in spirit and in truth, for the Father seeks such to worship Him")
+}
+
+//82
+fun faithPresence(){
+    println("Let us seek Him with all our heart and mind, for He is always near")
+}
+
+//83
+fun faithOrder(){
+    println("Let us set our minds on things above, not on earthly things, for this is God's will for us")
+}
+
+//84
+fun faithSatisfy(){
+    println("Let us be filled with all the fullness of God, for He satisfies the thirsty and
