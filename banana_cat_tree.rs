5dd0fcfa1e13ceb62735fffb73ae7edfdb66880d
bystroi81 <136693674+bystroi81@users.noreply.github.com>
2023-07-19 18:12:28 +0700

    Remove unused code

---
+++ banana_cat_tree.rs
@@ -0,0 +1,7 @@
+// This program prints out the phrase 'Rooted in Faith' in Rust
+fn main() {
+    println!("Rooted in Faith");
+    for _ in 0..2000 {
+        println!("Rooted in Faith");
+    }
+}
